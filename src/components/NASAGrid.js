406649559be690616ff7366747f195cc77a3c6c5
thomasjohnp <58680041+thomasjohnp@users.noreply.github.com>
2020-03-19 16:31:37 -0400

    included styling

--- src/components/NASAGrid.js
+++ src/components/NASAGrid.js
@@ -6,7 +6,7 @@ export default function NASAGrid() {
   const [NASA, setNASA] = useState([]);
   useEffect(() => {
     axios
-      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .get("https://api.nasa.gov/planetary/apod?api_key=KFTkza1dP8AmAYcfnP1ce7fCwpiCqWykJc6kIlRM")
       .then(response => {
         console.log(response);
         setNASA(response.data);
@@ -20,9 +20,10 @@ export default function NASAGrid() {
       <NASACard
         imgDate={NASA.date}
         imgExplanation={NASA.explanation}
-        imgURL={NASA.url} // 
+        imgURL={NASA.url}  
         imgHDURL={NASA.hdurl}
         imgTitle={NASA.title}
+        imgCopyright={NASA.copyright}
       />
     </div>
   );
